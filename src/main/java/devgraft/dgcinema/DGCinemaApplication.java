5f4d77fbc4148b49fdf3719b83f9fa7122b7d4dc
TAE HEON KWON <PCloud63514@gmail.com>
2023-04-09 22:15:44 +0900

    feat: springboot 2.7.3 기본 프로젝트 추가

---
+++ src/main/java/devgraft/dgcinema/DGCinemaApplication.java
@@ -0,0 +1,13 @@
+package devgraft.dgcinema;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DGCinemaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DGCinemaApplication.class, args);
+    }
+
+}
