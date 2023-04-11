ce22f284961749fa3abd21d7ee61c7672122052b
TAE HEON KWON <PCloud63514@gmail.com>
2023-04-11 14:02:01 +0900

    style(sql) sql 파일 수정

--- src/main/resources/sql/schema-h2.sql
+++ src/main/resources/sql/schema-h2.sql
@@ -1,81 +1,77 @@
-CREATE TABLE IF NOT EXISTS 'user' (
-    `id` INT(10) UNSIGNED NOT NULL,
-    `email` VARCHAR(255) NOT NULL,
-    `password` VARCHAR(255) NOT NULL
-    `phone` VARCHAR(255) NOT NULL,
-    PRIMARY KEY (`id`),
-    INDEX `IDX_EMAIL` (`email`)
-)
-ENGINE=InnoDB
-;
+CREATE TABLE IF NOT EXISTS "user"
+(
+    id       INT AUTO_INCREMENT PRIMARY KEY,
+    email    VARCHAR(255) NOT NULL,
+    password VARCHAR(255) NOT NULL,
+    phone    VARCHAR(255) NOT NULL,
+    PRIMARY KEY (id)
+);
+CREATE INDEX idx_user_email ON "user" (email);
 
-CREATE TABLE IF NOT EXISTS 'movie' (
-    `id` INT(10) UNSIGNED NOT NULL,
-    `title` VARCHAR(255) NOT NULL,
-    `description` VARCHAR(255) NOT NULL,
-    `bannerUrl` VARCHAR(255) NOT NULL,
-    `genre` VARCHAR(255) NOT NULL,
-    PRIMARY KEY (`id`)
-)
-ENGINE=InnoDB
-;
+CREATE TABLE IF NOT EXISTS "movie"
+(
+    id          INT AUTO_INCREMENT PRIMARY KEY,
+    title       VARCHAR(255) NOT NULL,
+    description VARCHAR(255) NOT NULL,
+    bannerUrl   VARCHAR(255) NOT NULL,
+    genre       VARCHAR(255) NOT NULL,
+    duration    INT          NOT NULL
+);
 
-CREATE TABLE IF NOT EXISTS 'theater' (
-    `id` INT(10) UNSIGNED NOT NULL,
-    `name` VARCHAR(255) NOT NULL,
-    PRIMARY KEY (`id`)
-)
-ENGINE=InnoDB
-;
+CREATE TABLE IF NOT EXISTS "theater"
+(
+    id   INT AUTO_INCREMENT PRIMARY KEY,
+    name VARCHAR(255) NOT NULL
+);
 
-CREATE TABLE IF NOT EXISTS 'seat' (
-    `id` INT(10) UNSIGNED NOT NULL,
-    `theaterId` INT(10) UNSIGNED NOT NULL,
-    `row` VARCHAR(255) NOT NULL,
-    `column` INT(10) NOT NULL,
-    PRIMARY KEY (`id`),
-    INDEX `IDX_THEATER_ID` (`theaterId`),
-    INDEX `IDX_ROW_COLUMN` (`row`, `column`)
-)
-ENGINE=InnoDB
-;
+CREATE TABLE IF NOT EXISTS "seat"
+(
+    id        INT AUTO_INCREMENT PRIMARY KEY,
+    theaterId INT          NOT NULL,
+    row       VARCHAR(255) NOT NULL,
+    column    INT          NOT NULL
+);
 
-CREATE TABLE IF NOT EXISTS 'showing' (
-    `id` INT(10) UNSIGNED NOT NULL,
-    `movieId` INT(10) UNSIGNED NOT NULL,
-    `theaterId` INT(10) UNSIGNED NOT NULL,
-    `date` date NOT NULL,
-    `time` time NOT NULL,
-    PRIMARY KEY (`id`),
-    INDEX `IDX_MOVIE_ID` (`movieId`),
-    INDEX `IDX_THEATER_ID` (`theaterId`),
-    INDEX `IDX_DATE` (`date`)
-)
-ENGINE=InnoDB
-;
+CREATE INDEX idx_seat_theater ON "seat" (theaterId);
+CREATE INDEX idx_seat_row_column ON "seat" (row, column);
 
-CREATE TABLE IF NOT EXISTS 'reservation' (
-    `id` INT(10) UNSIGNED NOT NULL,
-    `userId` INT(10) UNSIGNED NOT NULL,
-    `showingId` INT(10) UNSIGNED NOT NULL,
-    `seatId` INT(10) UNSIGNED NOT NULL,
-    `createAt` timestamp NOT NULL,
-    PRIMARY KEY (`id`),
-    INDEX `IDX_USER_ID` (`userId`)
-)
-ENGINE=InnoDB
-;
+CREATE TABLE IF NOT EXISTS "showing"
+(
+    id        INT AUTO_INCREMENT PRIMARY KEY,
+    movieId   INT            NOT NULL,
+    theaterId INT            NOT NULL,
+    showDate  date           NOT NULL,
+    startTime time           NOT NULL,
+    endTime   time           NOT NULL,
+    price     decimal(10, 2) NOT NULL
+);
 
-CREATE TABLE IF NOT EXISTS 'payment' (
-    `id` INT(10) UNSIGNED NOT NULL,
-    `reservationId` INT(10) UNSIGNED NOT NULL,
-    `status` VARCHAR(255) NOT NULL,
-    `amount` decimal(19, 0) NOT NULL,
-    `paymentMethod` VARCHAR(255) NOT NULL,
-    `createAt` timestamp NOT NULL,
-    `completedAt` timestamp NOT NULL,
-    PRIMARY KEY (`id`),
-    INDEX `IDX_RESERVATION_ID` (`reservationId`)
-)
-ENGINE=InnoDB
-;
+CREATE INDEX idx_showing_movie ON "showing" (movieId);
+CREATE INDEX idx_showing_theater ON "showing" (theaterId);
+CREATE INDEX idx_showing_date ON "showing" (showDate);
+
+CREATE TABLE IF NOT EXISTS "reservation"
+(
+    id        INT AUTO_INCREMENT PRIMARY KEY,
+    userId    INT       NOT NULL,
+    showingId INT       NOT NULL,
+    seatId    INT       NOT NULL,
+    createAt  timestamp NOT NULL
+);
+
+CREATE INDEX idx_reservation_user ON "reservation" (userId);
+CREATE INDEX idx_reservation_showing ON "reservation" (showingId);
+CREATE INDEX idx_reservation_seat ON "reservation" (seatId);
+
+CREATE TABLE IF NOT EXISTS "payment"
+(
+    id            INT AUTO_INCREMENT PRIMARY KEY,
+    reservationId INT            NOT NULL,
+    status        VARCHAR(255)   NOT NULL,
+    amount        decimal(10, 2) NOT NULL,
+    paymentMethod VARCHAR(255)   NOT NULL,
+    createAt      timestamp      NOT NULL,
+    completedAt   timestamp      NOT NULL
+);
+
+CREATE INDEX idx_payment_reservation ON "payment" (reservationId);
