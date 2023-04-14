7ac8a32b7170d85b3fae87e43306696a3476ba6d
TAE HEON KWON <PCloud63514@gmail.com>
2023-04-14 17:32:29 +0900

    test

--- dg-cinema-presentation/src/test/kotlin/devgraft/dgcinema/adapter/in/query/MovieQueryApiTest.kt
+++ dg-cinema-presentation/src/test/kotlin/devgraft/dgcinema/adapter/in/query/MovieQueryApiTest.kt
@@ -15,19 +15,23 @@ import org.mockito.Mockito.anyLong
 import org.mockito.Mockito.times
 import org.mockito.Mockito.verify
 import org.springframework.restdocs.mockmvc.RestDocumentationRequestBuilders.get
+import org.springframework.restdocs.payload.JsonFieldType
 import org.springframework.restdocs.payload.PayloadDocumentation.fieldWithPath
 import org.springframework.restdocs.payload.PayloadDocumentation.responseFields
 import org.springframework.restdocs.request.RequestDocumentation.parameterWithName
 import org.springframework.restdocs.request.RequestDocumentation.pathParameters
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers.status
+import java.time.LocalDate
 import java.time.format.DateTimeFormatter
 
-class MovieQueryApiTest  : RestDocsApiTest() {
+class MovieQueryApiTest : RestDocsApiTest() {
     @InjectMocks
     private lateinit var movieQueryApi: MovieQueryApi
+
     @Mock
     private lateinit var mockMovieSearchUseCase: MovieSearchUseCase
+
     @Captor
     private lateinit var movieIdCaptor: ArgumentCaptor<Long>
     override fun api() = arrayOf<Any>(movieQueryApi)
@@ -53,20 +57,22 @@ class MovieQueryApiTest  : RestDocsApiTest() {
             .andExpect(jsonPath("$.genre").value(givenMovie.genre))
             .andExpect(jsonPath("$.releaseDate").value(givenMovie.releaseDate.format(DateTimeFormatter.ofPattern("yyyy-MM-dd"))))
             .andExpect(jsonPath("$.duration").value(givenMovie.duration))
-            .andDo(document(
-                pathParameters(
-                    parameterWithName("movieId").description("영화 아이디")
-                ),
-                responseFields(
-                    fieldWithPath("movieId").description("영화 아이디"),
-                    fieldWithPath("title").description("영화 제목"),
-                    fieldWithPath("description").description("영화 설명"),
-                    fieldWithPath("bannerUrl").description("영화 배너 URL"),
-                    fieldWithPath("genre").description("영화 장르"),
-                    fieldWithPath("releaseDate").description("영화 개봉일"),
-                    fieldWithPath("duration").description("영화 길이(1분)")
+            .andDo(
+                document(
+                    pathParameters(
+                        parameterWithName("movieId").description("영화 아이디")
+                    ),
+                    responseFields(
+                        fieldWithPath("movieId").type(JsonFieldType.NUMBER).description("영화 아이디"),
+                        fieldWithPath("title").type(JsonFieldType.STRING).description("영화 제목"),
+                        fieldWithPath("description").type(JsonFieldType.STRING).description("영화 설명"),
+                        fieldWithPath("bannerUrl").type(JsonFieldType.STRING).description("영화 배너 URL"),
+                        fieldWithPath("genre").type(JsonFieldType.STRING).description("영화 장르"),
+                        fieldWithPath("releaseDate").type(LocalDate::class).description("영화 개봉일"),
+                        fieldWithPath("duration").type(JsonFieldType.NUMBER).description("영화 길이(1분)")
+                    )
                 )
-            ))
+            )
     }
 
     @Test
@@ -97,6 +103,7 @@ class MovieQueryApiTest  : RestDocsApiTest() {
             .andExpect(jsonPath("$.movies[0].description").value(givenMovie.description))
             .andExpect(jsonPath("$.movies[0].bannerUrl").value(givenMovie.bannerUrl))
             .andExpect(jsonPath("$.movies[0].genre").value(givenMovie.genre))
+            .andExpect(jsonPath("$.movies[0].duration").value(givenMovie.duration))
             .andExpect(
                 jsonPath("$.movies[0].releaseDate").value(
                     givenMovie.releaseDate.format(
@@ -106,6 +113,5 @@ class MovieQueryApiTest  : RestDocsApiTest() {
                     )
                 )
             )
-            .andExpect(jsonPath("$.movies[0].duration").value(givenMovie.duration))
     }
 }
