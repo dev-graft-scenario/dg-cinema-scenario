c2379f78927c04b549f4bcfaf2d5c358ab0c3775
TAE HEON KWON <PCloud63514@gmail.com>
2023-04-16 22:09:25 +0900

    style(index.html): dg-cinema rest-docs 문서 feat(build.gradle.kts): restDocs 관련 스크립트 수정

--- dg-cinema-presentation/build.gradle.kts
+++ dg-cinema-presentation/build.gradle.kts
@@ -1,10 +1,11 @@
-import org.asciidoctor.gradle.jvm.AsciidoctorTask
-
 plugins {
     id("org.asciidoctor.jvm.convert") version "3.3.2"
 }
 
+val asciidoctorExt: Configuration by configurations.creating
+
 dependencies {
+
     implementation(project(":dg-cinema-domain"))
     testImplementation(testFixtures(project(":dg-cinema-domain")))
     implementation(project(":dg-cinema-infrastructure"))
@@ -14,20 +15,38 @@ dependencies {
     runtimeOnly("com.h2database:h2")
     testRuntimeOnly("com.h2database:h2")
     testImplementation("org.springframework.restdocs:spring-restdocs-mockmvc")//:2.0.5.RELEASE
+    asciidoctorExt("org.springframework.restdocs:spring-restdocs-asciidoctor")
 }
 
-tasks.named("asciidoctor", AsciidoctorTask::class) {
-    dependsOn("test")
-    setSourceDir("src/docs/asciidoc")
-    setOutputDir(layout.buildDirectory.dir("docs/asciidoc").get().asFile)
-}
 
-tasks.named("build") {
-    dependsOn("asciidoctor")
+tasks {
+    val snippetsDir = file("build/generated-snippets")
+    val copyDocumentOutputDir = "../docs"//"src/main/resources/static/docs"
+
+    test {
+        outputs.dir(snippetsDir)
+        useJUnitPlatform()
+    }
+    asciidoctor {
+        configurations(asciidoctorExt.name)
+        inputs.dir(snippetsDir)
+        dependsOn(test)
+    }
+    build {
+        dependsOn(asciidoctor)
+    }
+
+    bootJar {
+        dependsOn(findByName("copyDocument"))
+    }
+
+    register("copyDocument", Copy::class) {
+        from(file("build/docs/asciidoc"))
+        into(file(copyDocumentOutputDir))
+        dependsOn(asciidoctor)
+    }
+
+    clean {
+        delete(files(copyDocumentOutputDir))
+    }
 }
-/**
- * 빌드하면 adoc파일들 잔뜩 나옴.
- * 이걸 한대 묶어서 내가 movie.adoc 이렇게 정의함
- * 이걸 html로 변환해야하니 특정 위치에 지정해야함
- * 뽑으면 어디에 html이 나올지 지정해야함
- */
