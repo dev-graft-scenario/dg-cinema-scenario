bf0ea7f5e18bfe0d8e1c52d6cda2183b9b301810
TAE HEON KWON <PCloud63514@gmail.com>
2023-04-17 21:17:31 +0900

    feat(AuditoriumAdapter): Port 구현 feat(AuditoriumRepository, AuditoriumEntity): JPA Repository 추가 test(AuditoriumEntityFixture): Fixture 추가

--- dg-cinema-infrastructure/src/main/kotlin/devgraft/dgcinema/adapter/out/AuditoriumAdapter.kt
+++ dg-cinema-infrastructure/src/main/kotlin/devgraft/dgcinema/adapter/out/AuditoriumAdapter.kt
@@ -3,10 +3,23 @@ package devgraft.dgcinema.adapter.out
 import devgraft.dgcinema.domain.model.Auditorium
 import devgraft.dgcinema.domain.ports.out.AuditoriumPort
 import org.springframework.stereotype.Component
+import java.util.stream.Collectors
 
 @Component
-class AuditoriumAdapter : AuditoriumPort {
+internal class AuditoriumAdapter(
+        private val auditoriumRepository: AuditoriumRepository
+) : AuditoriumPort {
     override fun findAllByTheaterId(theaterId: Long): List<Auditorium> {
-        return listOf(Auditorium(1, 1, ""))
+        return auditoriumRepository.findAllByTheaterId(theaterId).stream()
+                .map(::toAuditorium)
+                .collect(Collectors.toUnmodifiableList())
+    }
+
+    private fun toAuditorium(entity: AuditoriumEntity): Auditorium {
+        return Auditorium(
+                id = entity.id!!,
+                theaterId = entity.theaterId,
+                name = entity.name
+        )
     }
 }
--- dg-cinema-infrastructure/src/test/kotlin/devgraft/dgcinema/adapter/out/AuditoriumAdapterTest.kt
+++ dg-cinema-infrastructure/src/test/kotlin/devgraft/dgcinema/adapter/out/AuditoriumAdapterTest.kt
@@ -1,11 +1,17 @@
 package devgraft.dgcinema.adapter.out
 
+import capture
+import devgraft.dgcinema.adapter.out.fixtures.anAuditoriumEntity
 import org.assertj.core.api.Assertions
 import org.junit.jupiter.api.Assertions.*
 import org.junit.jupiter.api.Test
 import org.junit.jupiter.api.extension.ExtendWith
+import org.mockito.ArgumentCaptor
+import org.mockito.BDDMockito
+import org.mockito.Captor
 import org.mockito.InjectMocks
 import org.mockito.Mock
+import org.mockito.Mockito.anyLong
 import org.mockito.Mockito.times
 import org.mockito.Mockito.verify
 import org.mockito.junit.jupiter.MockitoExtension
@@ -16,14 +22,21 @@ class AuditoriumAdapterTest {
     private lateinit var auditoriumAdapter: AuditoriumAdapter
     @Mock
     private lateinit var mockAuditoriumRepository: AuditoriumRepository
-
+    @Captor
+    private lateinit var theaterIdCaptor: ArgumentCaptor<Long>
 
     @Test
     fun findAllByTheaterId_return_value() {
-
+        val givenAuditoriumEntityList = listOf(anAuditoriumEntity().build())
+        BDDMockito.given(mockAuditoriumRepository.findAllByTheaterId(anyLong())).willReturn(givenAuditoriumEntityList)
         val result = auditoriumAdapter.findAllByTheaterId(1)
 
         Assertions.assertThat(result).isNotEmpty
+        for ((index, auditorium) in result.withIndex()) {
+            Assertions.assertThat(auditorium.id).isEqualTo(givenAuditoriumEntityList[index].id)
+            Assertions.assertThat(auditorium.theaterId).isEqualTo(givenAuditoriumEntityList[index].theaterId)
+            Assertions.assertThat(auditorium.name).isEqualTo(givenAuditoriumEntityList[index].name)
+        }
     }
 
     @Test
@@ -32,6 +45,7 @@ class AuditoriumAdapterTest {
 
         auditoriumAdapter.findAllByTheaterId(givenTheaterId)
 
-        verify(mockAuditoriumRepository, times(1)).findAll()
+        verify(mockAuditoriumRepository, times(1)).findAllByTheaterId(capture(theaterIdCaptor))
+        Assertions.assertThat(theaterIdCaptor.value).isEqualTo(givenTheaterId)
     }
 }
