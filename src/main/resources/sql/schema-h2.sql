3063ca501f44e3292ae00f6d522c01f7e1deb949
TAE HEON KWON <PCloud63514@gmail.com>
2023-04-18 11:15:26 +0900

    style(schema-h2.sql) 스키마 id 타입 INT -> BIGINT 수정

--- src/main/resources/sql/schema-h2.sql
+++ src/main/resources/sql/schema-h2.sql
@@ -1,17 +1,18 @@
 CREATE TABLE IF NOT EXISTS member
 (
-    id       INT AUTO_INCREMENT PRIMARY KEY,
-    email    VARCHAR(255) NOT NULL,
+    id       BIGINT AUTO_INCREMENT PRIMARY KEY,
+    email    VARCHAR(255) NOT NULL UNIQUE,
     password VARCHAR(255) NOT NULL,
-    phone    VARCHAR(255) NOT NULL,
+    phone    VARCHAR(255) NOT NULL UNIQUE,
     PRIMARY KEY (id)
 );
 
 CREATE INDEX idx_member_email ON member (email);
+CREATE INDEX idx_member_phone ON member (phone);
 
 CREATE TABLE IF NOT EXISTS movie
 (
-    id          INT AUTO_INCREMENT PRIMARY KEY,
+    id          BIGINT AUTO_INCREMENT PRIMARY KEY,
     title       VARCHAR(255) NOT NULL,
     description VARCHAR(255) NOT NULL,
     bannerUrl   VARCHAR(255) NOT NULL,
@@ -22,25 +23,25 @@ CREATE TABLE IF NOT EXISTS movie
 
 CREATE TABLE IF NOT EXISTS theater
 (
-    id   INT AUTO_INCREMENT PRIMARY KEY,
+    id   BIGINT AUTO_INCREMENT PRIMARY KEY,
     name VARCHAR(255) NOT NULL
 );
 
 CREATE TABLE IF NOT EXISTS auditorium
 (
-    id   INT AUTO_INCREMENT PRIMARY KEY,
-    theaterId  INT          NOT NULL,
-    name VARCHAR(255)       NOT NULL
+    id        BIGINT AUTO_INCREMENT PRIMARY KEY,
+    theaterId BIGINT       NOT NULL,
+    name      VARCHAR(255) NOT NULL
 );
 
 CREATE INDEX idx_auditorium_theater ON auditorium (theaterId);
 
 CREATE TABLE IF NOT EXISTS seat
 (
-    id         INT AUTO_INCREMENT PRIMARY KEY,
-    auditoriumId  INT          NOT NULL,
-    seatRow    VARCHAR(255) NOT NULL,
-    seatColumn INT          NOT NULL
+    id           BIGINT AUTO_INCREMENT PRIMARY KEY,
+    auditoriumId BIGINT       NOT NULL,
+    seatRow      VARCHAR(255) NOT NULL,
+    seatColumn   INT          NOT NULL
 );
 
 CREATE INDEX idx_seat_auditorium ON seat (auditoriumId);
@@ -48,13 +49,13 @@ CREATE INDEX idx_seat_row_column ON seat (seatRow, seatColumn);
 
 CREATE TABLE IF NOT EXISTS showing
 (
-    id        INT AUTO_INCREMENT PRIMARY KEY,
-    movieId   INT            NOT NULL,
-    auditoriumId INT            NOT NULL,
-    showDate  date           NOT NULL,
-    startTime time           NOT NULL,
-    endTime   time           NOT NULL,
-    price     decimal(10, 2) NOT NULL
+    id           BIGINT AUTO_INCREMENT PRIMARY KEY,
+    movieId      BIGINT         NOT NULL,
+    auditoriumId BIGINT         NOT NULL,
+    showDate     date           NOT NULL,
+    startTime    time           NOT NULL,
+    endTime      time           NOT NULL,
+    price        decimal(10, 2) NOT NULL
 );
 
 CREATE INDEX idx_showing_movie ON showing (movieId);
@@ -63,11 +64,11 @@ CREATE INDEX idx_showing_date ON showing (showDate);
 
 CREATE TABLE IF NOT EXISTS reservation
 (
-    id        INT AUTO_INCREMENT PRIMARY KEY,
-    memberId  INT       NOT NULL,
-    showingId INT       NOT NULL,
-    seatId    INT       NOT NULL,
-    createdAt  timestamp NOT NULL
+    id        BIGINT AUTO_INCREMENT PRIMARY KEY,
+    memberId  BIGINT    NOT NULL,
+    showingId BIGINT    NOT NULL,
+    seatId    BIGINT    NOT NULL,
+    createdAt timestamp NOT NULL
 );
 
 CREATE INDEX idx_reservation_member ON reservation (memberId);
@@ -76,8 +77,8 @@ CREATE INDEX idx_reservation_seat ON reservation (seatId);
 
 CREATE TABLE IF NOT EXISTS payment
 (
-    id              INT AUTO_INCREMENT PRIMARY KEY,
-    reservationId   INT            NOT NULL,
+    id              BIGINT AUTO_INCREMENT PRIMARY KEY,
+    reservationId   BIGINT         NOT NULL,
     status          VARCHAR(255)   NOT NULL,
     amount          decimal(10, 2) NOT NULL,
     paymentMethod   VARCHAR(255)   NOT NULL,
